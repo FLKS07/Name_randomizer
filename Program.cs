e89d95202103179b94516ab97784e6e88f30473a
FLKS07 <2007markes.jacky.pc@gmail.com>
2022-06-22 13:42:40 +0100

    22/06/2022

--- Program.cs
+++ Program.cs
@@ -33,10 +33,18 @@
     
        
         for(int i = 0; i < numberofnamesRandomized; i++){ // This is gona repeat how many names the user wanted to randomized
-            Console.WriteLine(NameList[randomnumber.Next(numberofNames)]);
+
+            if(numberofnamesRandomized == 1){ //If the desire output of the user equals one(1)
+                Console.WriteLine(NameList[randomnumber.Next(numberofNames)]); //Outputs a random number of the list
+            }
+            else{
+                string name1 = NameList[randomnumber.Next(NameList.Count)]; //Sets name1 to a random string  of the list
+                Console.WriteLine(name1); //Outputs the string
+                NameList.Remove(name1); //Removes the same string on the list to not GET the same output twice
+            }
         }
             
-        Exit(); // This is a function to close the screen
+        Exit(); // This is a function to close the dscreen
     }
 
     public static void Exit(){ //Function to close the console when the user inputs any letter or number
