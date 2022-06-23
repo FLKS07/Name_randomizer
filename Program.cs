74835a7259716dc9d454800e1de7461f88bdb4fa
FLKS07 <2007markes.jacky.pc@gmail.com>
2022-06-24 00:09:03 +0100

    24/06/2022
    Now the user can't ask for more outputs than inputs.

--- Program.cs
+++ Program.cs
@@ -3,26 +3,34 @@
     private static void Main(string[] args)
     {
 
-        Console.Title = "Name randomizer 1.2 Made by: FLKS";
+        Console.Title = "Name randomizer 1.2 Made by: FLKS";                                           //Changes the title of the window
 
-        List<string> NameList = new List<string>();                                                    //Create a list
+        List<string> NameList = new List<string>();                                                    //Create a list for the names that gona randomize
         
         Random randomnumber = new Random();                                                            //Generator random numbers
         bool alreadyTyped; alreadyTyped = false;                                                       // Bool to see if the user as already typed a name
         
         
-        Console.WriteLine("Type here how many names you want to randomize:");
-        Console.Write(" "); int numberofNames = Convert.ToInt32(Console.ReadLine());
-        Thread.Sleep(300); Console.Clear();                                                            //Clear screen
-        Console.WriteLine("How many output names you want:");
-        Console.Write(" "); int numberofnamesRandomized = Convert.ToInt32(Console.ReadLine());
-        Thread.Sleep(300); Console.Clear();                                                            //Clear screen
+        Console.WriteLine("Type here how many names you want to randomize:");                          // Ask the user how many names he wants to randomize
+        Console.Write(" "); int numberofNames = Convert.ToInt32(Console.ReadLine());                   // Input for how many names the user want's to randomize
+        ClearScreen();                                                                                 // Calls a function to clear the screen
+
+
+        Console.WriteLine("How many output names you want:");                                          // Ask how many names the user want's randomize
+        Console.Write(" "); int numberofnamesRandomized = Convert.ToInt32(Console.ReadLine());         // Input for how many names he wants randomized
+        while(numberofnamesRandomized > numberofNames){                                                // If the number of names to randomize are greater than the number
+            Console.ForegroundColor = ConsoleColor.DarkRed;                                            // Changes the color of the letter on the console
+            Console.WriteLine("That can't be right. Type how many names you want to randomize:");      // Of names avaliable, is gona repeat these 2 lines
+            Console.ForegroundColor = ConsoleColor.Gray;                                               // Changes the color of the letter on the console
+            Console.Write(" ");  numberofnamesRandomized = Convert.ToInt32(Console.ReadLine());
+            
+        }
+
+        ClearScreen();                                                                                 // Calls a function to clear the screen
         
         Console.ForegroundColor = ConsoleColor.Red;
         for (int i = 0; i < numberofNames; i++){                                                       //This is gona repeat the names the user wanted to randomized
             
-           
-
             if(alreadyTyped == false){                                                                 //Condition to not show the same message every time
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Type here the names you want to randomize:");
@@ -34,7 +42,7 @@
             alreadyTyped = true;                                                                       // This is foor a condtion in line 20
         }
         
-        Thread.Sleep(300); Console.Clear();                                                            //Clear the console
+        ClearScreen();                                                                                 // Calls a function to clear the screen
         Console.WriteLine("Here is the final result:"); 
         Console.ForegroundColor = ConsoleColor.Green;                                                  // Sets the result to green
     
@@ -60,4 +68,10 @@
         Thread.Sleep(750); 
         Console.ReadLine();
     }
+
+    public static void ClearScreen(){                                                                  // Function to clear screen
+        Thread.Sleep(300); Console.Clear();
+    }
+
+   
 }
